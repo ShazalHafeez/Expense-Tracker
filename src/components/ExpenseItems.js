603aa7ef488bbf040e1aaacacc4ab8a16a4132f1
ShazaHafeez <shazal.masood@gmail.com>
2023-06-21 12:22:27 +0500

    initial

---
+++ src/components/ExpenseItems.js
@@ -0,0 +1,13 @@
+function ExpenseItems() {
+  return (
+    <div>
+      <div>Date</div>
+      <div>
+        <h2>Title</h2>
+        <div>Amount</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItems;
