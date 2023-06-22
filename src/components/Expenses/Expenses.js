d7c91fd8ab8abc3a8f837f8ab7244275ef3c148e
ShazaHafeez <shazal.masood@gmail.com>
2023-06-22 14:30:29 +0500

    Well structured Code

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,6 +1,6 @@
 import ExpenseItems from "./ExpenseItems";
 import "./Expenses.css"
-import Card from "./Card"
+import Card from "../UI/Card"
 export default function Expenses(props) {
   //console.log(props[0].amount);
 
